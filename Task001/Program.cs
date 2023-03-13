a25f9e4d57c9ffc03fe2d614ebdd81e89c6a2dcf
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-13 15:18:22 +0300

    add task001, task002 Homework2

---
+++ Task001/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите целое трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1000 && num > 99)
+    {
+        while (num >= 100) 
+        {
+            num/=10;    
+        }
+        var secondDig = num % 10;
+        Console.WriteLine(secondDig);
+    }   
+else 
+{
+    Console.WriteLine("Число не трёхзначное :( ");
+}
